1a7f734f66a0cdc7b4de4f8bcd7b7538d7475982
Edin Delanovic <edin.delanovic@smava.de>
2016-08-16 12:33:41 +0200

    Hot module replacement setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,22 +1,32 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
     devtool: 'eval-source-map',
-    entry: path.join(__dirname, '/client/index.js'),
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js')
+    ],
     output: {
-        path: '/'
+        path: '/',
+        publicPath: '/'
     },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 include: path.join(__dirname, 'client'),
-                loaders: ['babel']
+                loaders: ['react-hot','babel']
             }
         ]
     },
     resolve: {
-        extensions: ['','.js']
+        extensions: ['', '.js']
     }
 
 };
